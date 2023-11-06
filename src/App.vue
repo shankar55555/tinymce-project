391de2b73fce07d8926f50f457ccc56f5ec049c9
“Your <ashish@eligocs.com>
2023-11-06 15:38:05 +0530

    initally changes

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <editor :init="{
+      selector: '#basic-conf',
+      width: max-width,
+      height: 500,
+      plugins: [
+        'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+        'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media',
+        'table', 'emoticons', 'template', 'help'
+      ],
+      toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | ' +
+        'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+        'forecolor backcolor emoticons | help | link image | code',
+      menu: {
+        favs: { title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons' }
+      },
+      menubar: 'favs file edit view insert format tools table help',
+      content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+
+    }" />
+  </div>
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue'
+
+export default {
+  name: 'app',
+  components: {
+    'editor': Editor
+  }
+}
+</script>
